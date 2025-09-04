d586eb7fb43166f4cf0791d7c4307fa01fec85cc
chirag <100476437+chiragcode11@users.noreply.github.com>
2025-09-04 20:04:38 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="AI Interviewer API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://in-quiz-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
